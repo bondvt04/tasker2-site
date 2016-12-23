ce72461af65dbface417a2061f1d752e556779a5
AnatoliyBondar <bondvt04@gmail.com>
2016-12-23 10:59:30 +0200

    first commit

---
+++ experiments2/example2/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {Article} from "./article/article.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  getSortedArticles() {
+    return this.articles.sort((art1, art2) => art1.votes - art2.votes);
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    // console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    // console.log(title);
+    // return false;
+
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  public onButtonHover(e) {
+    console.log('hover', e);
+  }
+}
